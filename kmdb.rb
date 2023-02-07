b2e9014f1eb03e9d36e3d81e4d2250543714835c
MayankSharmaKellogg <mayank.sharma2@kellogg.northwestern.edu>
2023-02-07 00:55:38 +0000

    wip_again

--- kmdb.rb
+++ kmdb.rb
@@ -105,27 +105,27 @@ studio = Studio.new
 studio["name"] = "Warner Bros."
 studio.save
 
-studio_wb = Studio.find_by({"name" => "Warners Bros."})
+
 
 movie = Movie.new
 movie["title"] = "Batman Begins"
 movie["year_released"] = 2005
 movie["rated"] = "PG-13"
-movie["studio_id"] = studio_wb["id"]
+movie["studio_id"] = studio["id"]
 movie.save
 
 movie = Movie.new
 movie["title"] = "The Dark Knight"
 movie["year_released"] = 2008
 movie["rated"] = "PG-13"
-movie["studio_id"] = studio_wb["id"]
+movie["studio_id"] = studio["id"]
 movie.save
 
 movie = Movie.new
 movie["title"] = "The Dark Knight Rises"
 movie["year_released"] = 2012
 movie["rated"] = "PG-13"
-movie["studio_id"] = studio_wb["id"]
+movie["studio_id"] = studio["id"]
 movie.save
 
 #puts "movies: #{Movie.all.count}" # expected output - movies: 3
