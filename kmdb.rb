38886412de693be0ea48b47d16d076ee95a48aeb
MayankSharmaKellogg <mayank.sharma2@kellogg.northwestern.edu>
2023-02-07 02:31:46 +0000

    VOILA_done finally

--- kmdb.rb
+++ kmdb.rb
@@ -71,6 +71,103 @@
 # Use `Model.destroy_all` code.
 # TODO!
 
+# Generate models and tables, according to the domain model.
+# TODO!
+
+# Insert data into the database that reflects the sample data shown above.
+# Do not use hard-coded foreign key IDs.
+# TODO!
+
+# Prints a header for the movies output
+puts "Movies"
+puts "======"
+puts ""
+
+# Query the movies data and loop through the results to display the movies output.
+# TODO!
+
+# Prints a header for the cast output
+puts ""
+puts "Top Cast"
+puts "========"
+puts ""
+
+# Query the cast data and loop through the results to display the cast output for each movie.
+# TODO!
+
+# In this assignment, you'll be using the domain model from hw1 (found in the hw1-solution.sql file)
+# to create the database structure for "KMDB" (the Kellogg Movie Database).
+# The end product will be a report that prints the movies and the top-billed
+# cast for each movie in the database.
+
+# To run this file, run the following command at your terminal prompt:
+# `rails runner kmdb.rb`
+
+# Requirements/assumptions
+#
+# - There will only be three movies in the database – the three films
+#   that make up Christopher Nolan's Batman trilogy.
+# - Movie data includes the movie title, year released, MPAA rating,
+#   and studio.
+# - There are many studios, and each studio produces many movies, but
+#   a movie belongs to a single studio.
+# - An actor can be in multiple movies.
+# - Everything you need to do in this assignment is marked with TODO!
+
+# Rubric
+# 
+# There are three deliverables for this assignment, all delivered within
+# this repository and submitted via GitHub and Canvas:
+# - Generate the models and migration files to match the domain model from hw1.
+#   Table and columns should match the domain model. Execute the migration
+#   files to create the tables in the database. (5 points)
+# - Insert the "Batman" sample data using ruby code. Do not use hard-coded ids.
+#   Delete any existing data beforehand so that each run of this script does not
+#   create duplicate data. (5 points)
+# - Query the data and loop through the results to display output similar to the
+#   sample "report" below. (10 points)
+
+# Submission
+# 
+# - "Use this template" to create a brand-new "hw2" repository in your
+#   personal GitHub account, e.g. https://github.com/<USERNAME>/hw2
+# - Do the assignment, committing and syncing often
+# - When done, commit and sync a final time before submitting the GitHub
+#   URL for the finished "hw2" repository as the "Website URL" for the 
+#   Homework 2 assignment in Canvas
+
+# Successful sample output is as shown:
+
+# Movies
+# ======
+
+# Batman Begins          2005           PG-13  Warner Bros.
+# The Dark Knight        2008           PG-13  Warner Bros.
+# The Dark Knight Rises  2012           PG-13  Warner Bros.
+
+# Top Cast
+# ========
+
+# Batman Begins          Christian Bale        Bruce Wayne
+# Batman Begins          Michael Caine         Alfred
+# Batman Begins          Liam Neeson           Ra's Al Ghul
+# Batman Begins          Katie Holmes          Rachel Dawes
+# Batman Begins          Gary Oldman           Commissioner Gordon
+# The Dark Knight        Christian Bale        Bruce Wayne
+# The Dark Knight        Heath Ledger          Joker
+# The Dark Knight        Aaron Eckhart         Harvey Dent
+# The Dark Knight        Michael Caine         Alfred
+# The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
+# The Dark Knight Rises  Christian Bale        Bruce Wayne
+# The Dark Knight Rises  Gary Oldman           Commissioner Gordon
+# The Dark Knight Rises  Tom Hardy             Bane
+# The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
+# The Dark Knight Rises  Anne Hathaway         Selina Kyle
+
+# Delete existing data, so you'll start fresh each time this script is run.
+# Use `Model.destroy_all` code.
+# TODO!
+
 Movie.destroy_all
 Studio.destroy_all
 Actor.destroy_all
@@ -182,22 +279,22 @@ new_actor10.save
 
 
 
-movie_1 = Movie.where({"title" => "Batman Begins"})
-movie_2 = Movie.where({"title" => "Batman Begins"})
-movie_3 = Movie.where({"title" => "Batman Begins"})
+movie_1 = Movie.find_by({"title" => "Batman Begins"})
+movie_2 = Movie.find_by({"title" => "The Dark Knight"})
+movie_3 = Movie.find_by({"title" => "The Dark Knight Rises"})
 
 
-actor_1 = Actor.where({"name" => "Christian Bale"})
-actor_2 = Actor.where({"name" => "Michael Caine"})
-actor_3 = Actor.where({"name" => "Liam Neeson"})
-actor_4 = Actor.where({"name" => "Katie Holmes"})
-actor_5 = Actor.where({"name" => "Gary Oldman"})
-actor_6 = Actor.where({"name" => "Heath Ledger"})
-actor_7 = Actor.where({"name" => "Aaron Eckhart"})
-actor_8 = Actor.where({"name" => "Maggie Gyllenhaal"})
-actor_9 = Actor.where({"name" => "Tom Hardy"})
-actor_10 = Actor.where({"name" => "Joseph Gordon-Levitt"})
-actor_11 = Actor.where({"name" => "Anne Hathaway"})
+actor_1 = Actor.find_by({"name" => "Christian Bale"})
+actor_2 = Actor.find_by({"name" => "Michael Caine"})
+actor_3 = Actor.find_by({"name" => "Liam Neeson"})
+actor_4 = Actor.find_by({"name" => "Katie Holmes"})
+actor_5 = Actor.find_by({"name" => "Gary Oldman"})
+actor_6 = Actor.find_by({"name" => "Heath Ledger"})
+actor_7 = Actor.find_by({"name" => "Aaron Eckhart"})
+actor_8 = Actor.find_by({"name" => "Maggie Gyllenhaal"})
+actor_9 = Actor.find_by({"name" => "Tom Hardy"})
+actor_10 = Actor.find_by({"name" => "Joseph Gordon-Levitt"})
+actor_11 = Actor.find_by({"name" => "Anne Hathaway"})
 
 # Roles in Batman Begins
 
@@ -323,7 +420,7 @@ for movie in all_movies
     dtitle = movie["title"]
     dyear_released = movie["year_released"]
     drated = movie["rated"]
-    dstudio = Studio.where({"id" => movie["studio_id"]})
+    dstudio = Studio.find_by({"id" => movie["studio_id"]})
     dstudio_name = studio["name"]
     puts "#{dtitle}     #{dyear_released}       #{drated}       #{dstudio_name}"
 end
@@ -347,9 +444,9 @@ all_roles = Role.all
 #	TODO!
 
 for role in all_roles
-    movie = Movie.where({"id" => role["movie_id"]})
+    movie = Movie.find_by({"id" => role["movie_id"]})
     dmovie_title = movie["title"]
-    actor = Actor.where({"id" => role["actor_id"]})
+    actor = Actor.find_by({"id" => role["actor_id"]})
     dactor_name = actor["name"]
     dcharacter_name = role["character_name"]
     puts "#{dmovie_title}   #{dactor_name}   #{dcharacter_name}"
